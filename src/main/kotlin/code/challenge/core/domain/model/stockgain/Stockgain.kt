55ae8faa03240fc9f222e192503f0a6587454347
favanzo <fernando.avanzo@gmail.com>
2021-12-12 16:18:14 -0300

    function pmp implemented

--- src/main/kotlin/code/challenge/core/domain/model/stockgain/Stockgain.kt
+++ src/main/kotlin/code/challenge/core/domain/model/stockgain/Stockgain.kt
@@ -5,33 +5,44 @@ const val SELL = "sell"
 
 fun taxapply(operation: List<Operation>): List<Tax> = emptyList()
 
-fun weightedAveragePrice(operations: List<Operation>) = operations.let { operations ->
-    operations.fold(0.00) { sum, operation ->
-        when (operation.operation) {
-            BUY -> {
-                sum + totalOperationValue(operation.quantity, operation.unitCost)
-            }
-            else -> sum + 0
-        }
-    }.let { stockPrice ->
-        operations.filter { it.operation == BUY }.takeIf { it.isNotEmpty() }?.fold(0) { sum, operation ->
-            when (operation.operation) {
-                BUY -> {
-                    sum + operation.quantity
-                }
-                else -> sum + 0
-            }
-        }?.let { unitsBuyed ->
-            stockPrice / unitsBuyed
-        }?.let {
-            String.format("%.2f",it).toDouble()
-        } ?: stockPrice.let{
-            String.format("%.2f",it).toDouble()
-        }
-
-    }
+fun weightedAveragePrice(operations: List<Operation>) = sumOperations(
+    operations, 0.00,
+    { sum -> sum + 0 },
+    { operation, sum ->
+        sum + totalOperationValue(operation.quantity, operation.unitCost).toDouble()
+    }).run {
+    operations.filter { it.operation == BUY }.takeIf { it.isNotEmpty() }?.let {
+        sumOperations(it, 0,
+            { sum -> sum + 0 },
+            { operation, sum -> sum + operation.quantity })
+    }?.let {
+        (this / it).format()
+    } ?: this.format()
 }
 
-fun totalOperationValue(quantity: Int, unitcost: Number) =  unitcost.run {
+
+fun totalOperationValue(quantity: Int, unitcost: Number): Number = unitcost.run {
     toDouble() * quantity
+}
+
+private fun Double.format() = String.format("%.2f", this).toDouble()
+
+private fun <T : Number> sumOperations(
+    operations: List<Operation>,
+    sumStart: T,
+    actionFalse: (T) -> T,
+    actionTrue: (Operation, T) -> T
+): T = operations
+    .fold(sumStart) { sum, operation ->
+        buyOperation(operation, sum, actionFalse, actionTrue)
+    }
+
+private fun <T : Number> buyOperation(
+    operation: Operation,
+    acc: T,
+    actionFalse: (T) -> T,
+    actionTrue: (Operation, T) -> T
+): T = when (operation.operation) {
+    BUY -> actionTrue(operation, acc)
+    else -> actionFalse(acc)
 }
