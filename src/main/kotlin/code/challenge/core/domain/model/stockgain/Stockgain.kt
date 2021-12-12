b7ac15caf3ef136fda860a03584986e166ea2b98
favanzo <fernando.avanzo@gmail.com>
2021-12-12 16:28:56 -0300

    function pmp implemented

--- src/main/kotlin/code/challenge/core/domain/model/stockgain/Stockgain.kt
+++ src/main/kotlin/code/challenge/core/domain/model/stockgain/Stockgain.kt
@@ -2,17 +2,19 @@ package code.challenge.core.domain.model.stockgain
 
 const val BUY = "buy"
 const val SELL = "sell"
+const val VTO = 0.00
+const val QAN = 0
 
 fun taxapply(operation: List<Operation>): List<Tax> = emptyList()
 
-fun weightedAveragePrice(operations: List<Operation>) = sumOperations(
-    operations, 0.00,
+fun weightedAveragePrice(operations: List<Operation>) = sumList(
+    operations, VTO,
     { sum -> sum + 0 },
     { operation, sum ->
         sum + totalOperationValue(operation.quantity, operation.unitCost).toDouble()
     }).run {
     operations.filter { it.operation == BUY }.takeIf { it.isNotEmpty() }?.let {
-        sumOperations(it, 0,
+        sumList(it, QAN,
             { sum -> sum + 0 },
             { operation, sum -> sum + operation.quantity })
     }?.let {
@@ -27,17 +29,17 @@ fun totalOperationValue(quantity: Int, unitcost: Number): Number = unitcost.run 
 
 private fun Double.format() = String.format("%.2f", this).toDouble()
 
-private fun <T : Number> sumOperations(
+private fun <T : Number> sumList(
     operations: List<Operation>,
     sumStart: T,
     actionFalse: (T) -> T,
     actionTrue: (Operation, T) -> T
 ): T = operations
     .fold(sumStart) { sum, operation ->
-        buyOperation(operation, sum, actionFalse, actionTrue)
+        buyActions(operation, sum, actionFalse, actionTrue)
     }
 
-private fun <T : Number> buyOperation(
+private fun <T : Number> buyActions(
     operation: Operation,
     acc: T,
     actionFalse: (T) -> T,
