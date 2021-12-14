5daf95db11a01511e51e522b45686764c446abf6
favanzo <fernando.avanzo@gmail.com>
2021-12-14 17:55:22 -0300

    aplication testes implemeted

--- src/main/kotlin/code/challenge/Application.kt
+++ src/main/kotlin/code/challenge/Application.kt
@@ -1,5 +1,13 @@
 package code.challenge
 
-fun main(){
+import code.challenge.core.application.adapters.stdin.decodeOperations
+import code.challenge.core.application.adapters.stdout.encodeTaxs
+import code.challenge.core.domain.model.stockgain.taxrule
+
+fun main() {
     print("hellow world!!")
+}
+
+fun calcStocksGainTaxs(operations: String) = operations.run {
+    taxrule(decodeOperations()).encodeTaxs()
 }
--- src/test/kotlin/code/challenge/ApplicationTest.kt
+++ src/test/kotlin/code/challenge/ApplicationTest.kt
@@ -5,6 +5,13 @@ class ApplicationTest{
 
     @Test
     fun should_calc_stocks_gain_taxs(){
+        val given = """[{"operation":"buy", "unit-cost":10, "quantity": 100},{"operation":"sell", "unit-cost":15, "quantity": 50},{"operation":"sell", "unit-cost":15, "quantity": 50}]"""
 
+        val whendo = calcStocksGainTaxs(given)
+
+        val then = """[{"tax":0},{"tax":0},{"tax":0}]"""
+
+        assert(whendo == then)
+        { "${whendo}==${then}" }
     }
 }
