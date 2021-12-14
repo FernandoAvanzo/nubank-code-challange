2bd94bdb80262cada2fbdb69ef99f836fdc02ca6
favanzo <fernando.avanzo@gmail.com>
2021-12-14 18:46:25 -0300

    main function implemented

--- src/main/kotlin/code/challenge/Application.kt
+++ src/main/kotlin/code/challenge/Application.kt
@@ -4,9 +4,10 @@ import code.challenge.core.application.adapters.stdin.decodeOperations
 import code.challenge.core.application.adapters.stdout.encodeTaxs
 import code.challenge.core.domain.model.stockgain.taxrule
 
-fun main() {
-    print("hellow world!!")
-}
+fun main(args: Array<String>): Unit = args
+    .takeIf { it.isNotEmpty() }?.forEach {
+        println(calcStocksGainTaxs(it))
+    } ?: Unit
 
 fun calcStocksGainTaxs(operations: String) = operations.run {
     taxrule(decodeOperations()).encodeTaxs()
